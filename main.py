251011a2c594917a8792c9c096ffe20d1b37c51d
Kaustubh <81306562+kaustubh187@users.noreply.github.com>
2023-06-05 01:14:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, world!'
+    return null
 
 @app.route('/trigger_report')
 def trigger_report():
-    return "hello"
+    createreport()
+    return 729381012929
 
 
 @app.route('/get_report/<reportid>')
 def get_report(report_id):
-    return report_id
+    
+    return "Incomplete"
+
+
+def createreport()
+# Function to create report
 app.run(debug=True)
